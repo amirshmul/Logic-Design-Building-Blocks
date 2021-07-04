0d77b447060552cfad21bf57369ba5fc0e9496fd
amirshmul <86232400+amirshmul@users.noreply.github.com>
2021-07-04 18:35:39 +0300

    Update NBitLsbPriorityEncoder.sv

--- N-Bit Round Rubin Arbiter/RTL_src/NBitLsbPriorityEncoder.sv
+++ N-Bit Round Rubin Arbiter/RTL_src/NBitLsbPriorityEncoder.sv
@@ -18,6 +18,21 @@ module NBitLsbPriorityEncoder #(parameter W_DATA=32)(
         compensating for lsb_priority='0 case*/
         for(int i=W_DATA; i>0; i=i-1) begin
             `case_generate(i-1, bitline_in, lsb_priority);
+            // Amir: please dont use defines and macros :)
+            // There is a cleaner way to write this code.
+        
+        always_comb begin
+          lsb_priority = '0;
+            for (integer i=0; i < W_DATA; i++) begin
+                if (bitline_in[i] == 1'b1) begin
+                    lsb_priority[i] = 1'b1;
+                    break;
+                end
+            end
+        end
+            
+
+          
         end
     end
 endmodule
@@ -32,11 +47,15 @@ endmodule
    can be handy on large designs, but the second one
    keeps the code cleaner and more pleasant to read.
    
+   //Amir: it is a metter of style. I prefer the second one, from the reasons you mentioned above.
+   
 2. tried using the "priority case" and "priority if"
 couldnt find a way to put a for loop inside the
 the mentioned blocks, is there any way to construct
 a dynamic case block where the number of cases 
 and the conditions are parameter dependant?
+
+// Amir: Right, case is not the right coding style when writing "Generic" prio-encoder
 */
 
 /*acknoledgments*/
